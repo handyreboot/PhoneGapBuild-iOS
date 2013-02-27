c60bad91e01f678970b087656ab3e742f4bb67cf
Robert Winterbottom <rwinterbottom@blueraster.com>
2013-02-27 16:05:11 -0500

    Preparing new version

---
+++ JS/main.js
@@ -0,0 +1,34 @@
+// Initialize PhoneGap API then initialize the App
+var phonegapAPI = (function() {
+	return {	
+		initialize: function() {
+			// Check to make sure the PhoneGap JS is ready to use before attempting to use it
+			document.addEventListener('deviceready',this.deviceReady,false);
+		},
+		deviceReady: function() {
+			alert('Ready to use PhoneGap API.');
+      //onorientationchange doesn't always fire in a timely manner in Android so check for both orientationchange and resize
+      var supportsOrientationChange = "onorientationchange" in window, orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
+      window.addEventListener(orientationEvent, function() {
+        if(map) {
+          map.reposition();
+          map.resize();
+        }
+      }, false);
+			app.initialize();
+		}
+	} // End of returned object
+})();
+
+var app = {
+		initialize: function() {
+			//Begin Here
+      app.gaPlugin = window.plugins.gaPlugin;
+      app.gaPlugin.init(function(){alert('GA Ready!');},function(error){},"UA-5275645-4",10);
+	  },
+
+    gaEvent: function() {
+      app.gaPlugin.trackEvent(function(){alert('Event Tracked!')},function(){alert('failure')},"Link","Click","Clicked Test Link",1);
+    }
+
+};
