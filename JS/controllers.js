628185d0efb6bdb0021b5900b5e34684311cbb5c
Robert Winterbottom <rwinterbottom@blueraster.com>
2013-03-15 11:57:17 -0400

    tweaked custom popup content

--- JS/controllers.js
+++ JS/controllers.js
@@ -758,7 +758,7 @@ function MappingController($http,$timeout,$scope) {
                 }
               });
             });
-            content = $scope.currentIndicator+" - "+item.year+": <br>"+item.val+"<br><br><div id='chartDiv' class='chartPopup'></div>";
+            content = $scope.currentIndicator+" - "+item.year+": "+item.val+"<br><br><div id='chartDiv' class='chartPopup'></div>";
             // item.label,content
             document.getElementById("myInfoWindow").style.display = "block";
             document.getElementById("myInfoWindowTitle").innerHTML = item.label;
